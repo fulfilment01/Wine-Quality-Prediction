616e0baa8273167df71fc496dc6dc18dbdcd6b38
fulfilment01 <tobiowolabi283@gmail.com>
2025-02-27 12:48:12 +0100

    exception, logger and utils added

---
+++ src/wine-quality-prediction/utils/common.py
@@ -0,0 +1,89 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from wine-quality-prediction import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any 
+
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml) -> ConfigBox:
+    """
+    Read a YAML file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loadded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e 
+    
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """
+    create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directorie
+        ignore_log (bool): ignore if multiple dir is to be created. Default to False    
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"directory: {path} created successfully")
+
+
+@ensure_annotations
+def save_to_json(path: Path, data: dict):
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+    logger.info(f"json file saved at: {path}")
+
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file loaded successfully from: {path}")
+    return ConfigBox(content)
+
+
+@ensure_annotations
+def save_bin(data: Any, path:Path):
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at: {path}")
+
+
+@ensure_annotations
+def load_bin(path:Path) -> Any:
+    data=joblib.load(path)
+    logger.info(f"binary file loaded from: {path}")
+    return data
+
+
+
+@ensure_annotations
+def get_size(path:Path) -> str:
+    size_in_kb = round(os.path.getsize{path}/1024)
+    return f"~ {size_in_kb} KB"
