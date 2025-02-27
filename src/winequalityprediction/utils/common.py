98ad185f4a14edeeb433c5987c6b301dc5fea435
fulfilment01 <tobiowolabi283@gmail.com>
2025-02-27 14:20:59 +0100

    updated project

--- src/winequalityprediction/utils/common.py
+++ src/winequalityprediction/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from wine-quality-prediction import logger
+from winequalityprediction import logger
 import json
 import joblib
 from ensure import ensure_annotations
--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "wine-quality-prediction"
+project_name = "winequalityprediction"
 
 
 list_of_files = [
