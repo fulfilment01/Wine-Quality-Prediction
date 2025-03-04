567b85753d3a22b259ad3d7a71b319e319377cf5
fulfilment01 <tobiowolabi283@gmail.com>
2025-03-04 14:12:00 +0100

    data_transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from winequalityprediction import logger
 from winequalityprediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from winequalityprediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from winequalityprediction.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -22,3 +23,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
--- src/winequalityprediction/config/configuration.py
+++ src/winequalityprediction/config/configuration.py
@@ -1,5 +1,5 @@
 from winequalityprediction.constants import *
-from winequalityprediction.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from winequalityprediction.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from winequalityprediction.utils.common import read_yaml, create_directories
 
 class ConfigurationManager:
@@ -41,3 +41,16 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path = config.data_path,
+        )
+
+        return data_transformation_config
