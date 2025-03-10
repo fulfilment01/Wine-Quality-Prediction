7e66fdaf1dcadc468e71125c81ecf0483b393a9b
fulfilment01 <tobiowolabi283@gmail.com>
2025-03-10 12:46:29 +0100

    model training added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from winequalityprediction import logger
 from winequalityprediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from winequalityprediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from winequalityprediction.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from winequalityprediction.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -29,6 +30,16 @@ STAGE_NAME = "Data Transformation stage"
 try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
     obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+STAGE_NAME = "Model Training stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = ModelTrainerTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
 except Exception as e:
--- src/winequalityprediction/config/configuration.py
+++ src/winequalityprediction/config/configuration.py
@@ -1,5 +1,5 @@
 from winequalityprediction.constants import *
-from winequalityprediction.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from winequalityprediction.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from winequalityprediction.utils.common import read_yaml, create_directories
 
 class ConfigurationManager:
@@ -54,3 +54,23 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
+    
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path=config.train_data_path,
+            test_data_path=config.test_data_path,
+            model_name=config.model_name,
+            alpha=params.alpha,
+            l1_ratio=params.l1_ratio,
+            target_column=schema.name
+        )
+
+        return model_trainer_config
